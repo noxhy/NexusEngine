d45de0d3949fb4fd0437cf394979971ae3f8996a
Noah Descalzo <58011428+noxhy@users.noreply.github.com>
2025-07-28 16:08:51 -0400

    clear save button

--- scenes/instances/options/options_menu.gd
+++ scenes/instances/options/options_menu.gd
@@ -5,3 +5,7 @@ func _on_button_pressed():
 	
 	Global.stop_song()
 	Global.change_scene_to("res://scenes/options/offset_calibrator.tscn")
+
+
+func _on_clear_save_pressed() -> void:
+	SaveHandeler.init()
