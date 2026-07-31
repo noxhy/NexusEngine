1b823969302bfb6910534fb4567661bc43705bd9
noxhy <58011428+noxhy@users.noreply.github.com>
2026-07-31 03:05:20 -0400

    Update week.gd

--- noah/resources/week.gd
+++ noah/resources/week.gd
@@ -3,10 +3,4 @@ extends Resource
 class_name Week
 
 @export var week_name: String
-@export var week_animation: String
-
-## to do: make a better system for this, not likin it, doesnt adapt to name changes, etc
-@export var node_path: String
-
-@export_subgroup("Song List")
 @export var song_list: Array[Song]
