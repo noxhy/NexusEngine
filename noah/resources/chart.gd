64ba606762a757bb5daeccd4d6842361c738b6b0
noxhy <58011428+noxhy@users.noreply.github.com>
2026-08-22 21:47:46 -0400

    new format number

--- noah/resources/chart.gd
+++ noah/resources/chart.gd
@@ -2,7 +2,7 @@
 extends Resource
 class_name Chart
 
-const CURRENT_VERSION: int = 1
+const CURRENT_VERSION: int = 2
 
 enum ChartFormat {
 	VSLICE = 0,
@@ -21,19 +21,16 @@ static func chart_format_to_str(type:ChartFormat) -> String:
 		_: return "Undefined"
 
 @export_group("Chart Data")
-
+## Scroll speed of the arrows in gameplay
 @export_range(0.0, 5.0, 0.1) var scroll_speed = 1.0
+
 ## Audio latency.
 @export var offset = 0.0
-
-# Actual Chart Storage
-@export var chart_data = {
-	
-	"notes": [],
-	"events": [],
-	"tempos": {0.0: 60},
-	"meters": {0.0: [4, 4]},
-}
+@export var notes: Array = []
+@export var events: Array = []
+@export var tempos: Dictionary = {}
+@export var time_signatures: Dictionary = {}
+@export var chart_data: Dictionary = {}
 
 #this isnt a "great" way to handle versions but i cant justify doing anything more elaborate
 var version: int :
@@ -42,15 +39,25 @@ var version: int :
 	set(v):
 		chart_data.set('version', v)
 
-func get_notes_data() -> Array: return chart_data.get("notes")
-func get_events_data() -> Array: return chart_data.get("events")
-func get_tempos_data() -> Dictionary: return chart_data.get("tempos")
-func get_meters_data() -> Dictionary: return chart_data.get("meters")
+func get_notes_data() -> Array:
+	return chart_data.get("notes", notes)
 
-func merge_events_into_this(events:ChartEvents):
+func get_events_data() -> Array:
+	return chart_data.get("_events", events)
+
+func get_tempos_data() -> Dictionary:
+	return chart_data.get("tempos", tempos)
+
+func get_meters_data() -> Dictionary:
+	return chart_data.get("meters", time_signatures)
+
+func get_time_signature_data() -> Dictionary:
+	return chart_data.get("time_signatures", time_signatures)
+
+func merge_events_into_this(_events: ChartEvents):
 	var chart_events = get_events_data()
 	
-	chart_events.append_array(events.data)
+	chart_events.append_array(_events.data)
 	
 	var ret_events = [] 
 	var push = func(ev:Variant): #clear out dupes
@@ -63,11 +70,12 @@ func merge_events_into_this(events:ChartEvents):
 		
 		ret_events.append(ev)
 	
-	for event in chart_events: push.call(event)
+	for event in chart_events:
+		push.call(event)
 	
 	ret_events.sort_custom(sort_notes)
 	
-	chart_data.set('events', ret_events)
+	chart_data.set('_events', ret_events)
 
 ## Returns the tempo at a given time
 func get_tempo_at(time: float) -> float:
@@ -124,32 +132,31 @@ static func load(path:String) -> Chart:
 			if json and json is Dictionary:
 				match resolve_chart_type(json):
 					ChartFormat.PSYCH:
-						var events = []
-						var events_file = FileAccess.open(path.get_base_dir() + '/events.json', FileAccess.READ)
+						var _events = []
+						var events_file = FileAccess.open(path.get_base_dir() + '/_events.json', FileAccess.READ)
 						
 						if events_file:
 							var events_json = JSON.parse_string(events_file.get_as_text())
 							if events_json:
-								if not events_json.has('events'):
+								if not events_json.has('_events'):
 									events_json = events_json.get('song')
 								
-								events = events_json.get('events', [])
-								
-						return convert_psych(json, events, false)
+								_events = events_json.get('_events', [])
+						return convert_psych(json, _events, false)
 					ChartFormat.PSYCH_V1:
-						var events = []
-						var events_file = FileAccess.open(path.get_base_dir() + '/events.json', FileAccess.READ)
+						var _events = []
+						var events_file = FileAccess.open(path.get_base_dir() + '/_events.json', FileAccess.READ)
 						
-						if events_file: #maybe check if the events file is cne ?
+						if events_file: #maybe check if the _events file is cne ?
 							var events_json = JSON.parse_string(events_file.get_as_text())
 							if events_json:
-								if not events_json.has('events'):
+								if not events_json.has('_events'):
 									events_json = events_json.get('song')
 								
-								events = events_json.get('events', [])
+								_events = events_json.get('_events', [])
 						
 						
-						return convert_psych(json, events)
+						return convert_psych(json, _events)
 						
 					ChartFormat.VSLICE:
 						var meta_path: String = path.get_basename()
@@ -167,20 +174,20 @@ static func load(path:String) -> Chart:
 						
 						assert(FileAccess.file_exists(meta_path), 'failed to find cne chart meta.json')
 						
-						var events: Array = []
-						var events_file = FileAccess.open(path.get_base_dir() + '/events.json', FileAccess.READ)
+						var _events: Array = []
+						var events_file = FileAccess.open(path.get_base_dir() + '/_events.json', FileAccess.READ)
 						
 						if events_file:
 							var events_json = JSON.parse_string(events_file.get_as_text())
 							if events_json:
 								if events_json is Dictionary:
-									if events_json.has('events'):
-										events = events_json.get('events')
+									if events_json.has('_events'):
+										_events = events_json.get('_events')
 						
 						var meta_file: FileAccess = FileAccess.open(meta_path, FileAccess.READ)
 						var meta_json = JSON.parse_string(meta_file.get_as_text())
 						if meta_json:
-							return convert_cne(json, meta_json, events)
+							return convert_cne(json, meta_json, _events)
 					_:
 						pass
 	
@@ -213,27 +220,41 @@ static func sort_notes(a, b) -> bool:
 static func update_chart(chart: Chart) -> Chart:
 	
 	if chart.version < CURRENT_VERSION:
-		
-		if chart.version == 0: #old chart
-			print('updating legacy chart')
-			var meters = chart.get_meters_data()
-			
-			for time in meters:
-				var meter_data = meters.get(time)
-				if int(meter_data[1]) == int(meter_data[0] * 4):
-					meter_data[1] /= meter_data[0]
-			
-			for note in chart.get_notes_data():
-				note[3] = str(note[3])
-			
-			chart.version = CURRENT_VERSION
+		print("updating chart version: ", chart.version)
+		match chart.version:
+			0: #old chart
+				var meters = chart.get_meters_data()
+				
+				for time in meters:
+					var meter_data = meters.get(time)
+					if int(meter_data[1]) == int(meter_data[0] * 4):
+						meter_data[1] /= meter_data[0]
+				
+				for note in chart.get_notes_data():
+					note[3] = str(note[3])
+				
+				chart.version = 1
+				update_chart(chart)
+			1: # v1 formatr
+				chart.notes = chart.get_notes_data()
+				chart.events = chart.get_events_data()
+				chart.tempos = chart.get_tempos_data()
+				chart.time_signatures = chart.get_meters_data()
+				
+				chart.chart_data.erase("notes")
+				chart.chart_data.erase("events")
+				chart.chart_data.erase("tempos")
+				chart.chart_data.erase("meters")
+				
+				chart.version = CURRENT_VERSION
+				update_chart(chart)
 		
 		
 		ResourceSaver.save(chart, chart.resource_path)
 	
 	return chart
 
-static func convert_psych(data:Dictionary,events:Array = [], v1:bool = true) -> Chart:
+static func convert_psych(data:Dictionary,_events:Array = [], v1:bool = true) -> Chart:
 	var chart = Chart.new()
 	
 	var note_data = []
@@ -306,10 +327,10 @@ static func convert_psych(data:Dictionary,events:Array = [], v1:bool = true) -> 
 		
 		section_time += seconds_per_measure
 	
-	if data.has('events'):
-		events.append_array(data.get('events'))
+	if data.has('_events'):
+		_events.append_array(data.get('_events'))
 	
-	for i in events:
+	for i in _events:
 		var time = i[0]
 		# Event name conversion
 		for j in i[1]:
@@ -349,7 +370,7 @@ static func convert_psych(data:Dictionary,events:Array = [], v1:bool = true) -> 
 	
 	chart.chart_data = {
 		"notes": note_data,
-		"events": event_data,
+		"_events": event_data,
 		"tempos": tempo_data,
 		"meters": meter_data
 	}
@@ -412,7 +433,7 @@ static func convert_vslice(data:Dictionary, meta:Dictionary,diff:String = '') ->
 	note_data.sort_custom(sort_notes)
 	
 	# Adding event data.
-	for i in data.get('events'):
+	for i in data.get('_events'):
 		var time = i.t / 1000.0
 		
 		var tempo = get_temp_at_struct.call(time, tempo_data)
@@ -456,14 +477,14 @@ static func convert_vslice(data:Dictionary, meta:Dictionary,diff:String = '') ->
 	
 	chart.chart_data = {
 		"notes": note_data,
-		"events": event_data,
+		"_events": event_data,
 		"tempos": tempo_data,
 		"meters": meter_data
 	}
 	
 	return chart
 
-static func convert_cne(data:Dictionary, meta:Dictionary, events:Array = []) -> Chart:
+static func convert_cne(data:Dictionary, meta:Dictionary, _events:Array = []) -> Chart:
 	var chart = Chart.new()
 	
 	var note_data = []
@@ -487,10 +508,10 @@ static func convert_cne(data:Dictionary, meta:Dictionary, events:Array = []) -> 
 	var current_bpm = meta.get('bpm')
 	tempo_data[0.0] = current_bpm
 	
-	if data.has('events'):
-		events.append_array(data.get('events'))
+	if data.has('_events'):
+		_events.append_array(data.get('_events'))
 	
-	for event_packet in events:
+	for event_packet in _events:
 		
 		var event = []
 		if event_packet.name == 'Camera Movement':
@@ -537,7 +558,7 @@ static func convert_cne(data:Dictionary, meta:Dictionary, events:Array = []) -> 
 	
 	chart.chart_data = {
 		"notes": note_data,
-		"events": event_data,
+		"_events": event_data,
 		"tempos": tempo_data,
 		"meters": meter_data
 	}
