1bb808e2de71b82d514551463facba83aaede6f9
noxhy <58011428+noxhy@users.noreply.github.com>
2026-09-05 21:01:27 -0400

    strum setters

--- noah/game/strumlines/strum_manager.gd
+++ noah/game/strumlines/strum_manager.gd
@@ -2,87 +2,78 @@
 extends Node2D
 class_name StrumManager
 
-@export var note_skin: NoteSkin = NoteSkin.new()
+@export var note_skin: NoteSkin: set = set_skin
 ## List of Nodes of the strumlines.
-@export var strums: Array[Strum] = []
+@export var strums: Array[Strum]
 ## Vocal track ID.
 @export var id: int = 0
 
 ## If [code]true[/code], the strumlines will read the player's input.
-@export var can_press: bool = true
+@export var can_press: bool: set = set_press
 ## If [code]true[/code], the strumlines will hit notes automatically. Typically used for botplay
 ## or the enemy strumlines.
-@export var auto_play: bool = false
+@export var auto_play: bool: set = set_auto_play
 ## If [code]true[/code], the strumlines will create a note splash effect when hitting or holding a
 ## note. Typically used for the player strumlines.
-@export var can_splash: bool = false
+@export var can_splash: bool: set = set_can_splash
 ## If [code]true[/code], the strumlines will count as a enemy strumline. Enemy strumlines do not
 ## affect player stats.
-@export var enemy_slot: bool = false
+@export var enemy_slot: bool: set = set_enemy_slot
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_skin(note_skin)
-	set_press(can_press)
-	set_auto_play(auto_play)
-	set_can_splash(can_splash)
-	set_enemy_slot(enemy_slot)
-	
 	var i: int = 0
 	for strum in strums:
 		strum.lane = i
 		i += 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta) -> void:
-	pass
-
-
-func set_skin(new_skin: NoteSkin):
+func set_skin(new_skin: NoteSkin) -> void:
+	note_skin = new_skin
 	for strum in strums:
 		strum.set_skin(new_skin)
 
 
-# PlayState Util
-
-
-func set_scroll_speed(new_scroll_speed: float):
+func set_scroll_speed(new_scroll_speed: float) -> void:
 	for strum in strums:
 		strum.scroll_speed = new_scroll_speed
 
 
-func set_scroll(new_scroll: float):
+func set_scroll(new_scroll: float) -> void:
 	for strum in strums:
 		strum.scroll = new_scroll
 
 
-func set_press(toggle: bool):
+func set_press(toggle: bool) -> void:
+	can_press = toggle
 	for strum in strums:
 		strum.can_press = toggle
 
 
-func set_auto_play(toggle: bool):
+func set_auto_play(toggle: bool) -> void:
+	auto_play = toggle
 	for strum in strums:
 		strum.auto_play = toggle
 
 
-func set_offset(offset: float):
+func set_offset(offset: float) -> void:
 	for strum in strums:
 		strum.offset = offset
 
 
-func set_can_splash(toggle: bool):
+func set_can_splash(toggle: bool) -> void:
+	can_splash = toggle
 	for strum in strums:
 		strum.can_splash = toggle
 
 
-func set_enemy_slot(toggle: bool):
+func set_enemy_slot(toggle: bool) -> void:
+	enemy_slot = toggle
 	for strum in strums:
 		strum.enemy_slot = toggle
 
 
-func set_ignored_note_types(_note_types: Array):
+func set_ignored_note_types(_note_types: Array) -> void:
 	for strum in strums:
 		strum.ignored_note_types = _note_types
 
@@ -90,14 +81,15 @@ func set_ignored_note_types(_note_types: Array):
 func get_strumline(lane: int) -> Strum:
 	return strums[lane]
 
+
 func get_scroll_speed(lane: int) -> float:
 	return get_strumline(lane).scroll_speed
 
 
-func create_note(time: float, lane: int, length: float, note_type: String, tempo: float):
+func create_note(time: float, lane: int, length: float, note_type: String, tempo: float) -> void:
 	strums[lane].create_note(time, length, note_type, tempo)
 
-func create_splash(lane: int, animation_name: StringName):
+func create_splash(lane: int, animation_name: StringName) -> void:
 	var anim_to_play: StringName = animation_name + &"_splash"
 	if animation_name.is_empty():
 		anim_to_play = strums[lane].strum_name + &"_splash"
@@ -105,9 +97,9 @@ func create_splash(lane: int, animation_name: StringName):
 	strums[lane].create_splash(anim_to_play)
 
 # Visual Util
-func glow_strum(lane: int):
+func glow_strum(lane: int) -> void:
 	strums[lane].glow_strum()
 
 
-func press_strum(lane: int):
+func press_strum(lane: int) -> void:
 	strums[lane].press_strum()
