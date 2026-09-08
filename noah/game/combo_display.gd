0234a006a31b03e1cd7be4d55c55f739d5babd4c
noxhy <58011428+noxhy@users.noreply.github.com>
2026-09-08 04:56:48 -0400

    Update combo_display.gd

--- noah/game/combo_display.gd
+++ noah/game/combo_display.gd
@@ -10,11 +10,12 @@ func _ready() -> void:
 	emitting = true
 	if numbers:
 		texture = ui_skin.numbers_texture.get_frame_texture(animation, 0)
+		scale = Vector2.ONE * ui_skin.numbers_scale
 	else:
 		texture = ui_skin.rating_texture.get_frame_texture(animation, 0)
+		scale = Vector2.ONE * ui_skin.rating_scale
 	
 	#offset = ui_skin.offsets.get(animation, Vector2.ZERO)
-	scale = Vector2.ONE * ui_skin.rating_scale
 	
 	if ui_skin.pixel_texture:
 		texture_filter = TEXTURE_FILTER_NEAREST
